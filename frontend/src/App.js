598e52825f68f62ae05b84e9c3aab9c1685342b9
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-05 15:46:55 -0500

    Added some data and got search working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import elasticlunr from 'elasticlunr';
 import { Button, Navbar, MenuItem, Nav, NavItem, NavDropdown, FormControl } from 'react-bootstrap';
+
+import termDump from './smallClasses.json'
+import searchIndex from './smallSearchIndex.json'
 
 import './bootstrap.css'
 import './bootstrap-theme.css'
@@ -11,13 +15,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      classes: []
+    }
+
     this.textBox = null;
+    console.log('Loading search index...')
+    this.index = elasticlunr.Index.load(searchIndex);
+    console.log('Done loading search index')
 
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   onSubmit() {
-    alert(this.textBox.value)
+
+    let results = this.index.search(this.textBox.value)
+
+    let classes = []
+
+    for (let result of results.slice(0, 10)) {
+      classes.push(termDump.classMap[result.ref])
+    }
+
+    this.setState({
+      classes: classes
+    })
   }
 
   render() {
@@ -39,20 +61,23 @@ class App extends Component {
         </Navbar.Collapse>
       </Navbar>
 
-      <div className="container">
- 
-
-          <form className="form">
-            <FormControl
-              className="input"
-              type='text'
-              placeholder='Enter text'
-              inputRef = {(textBox) => {this.textBox = textBox}}
-              onChange = { this.onSubmit } 
-            />
-          </form>
-
-      </div>
+        <div className="container">
+            <form className="form">
+              <FormControl
+                className="input"
+                type='text'
+                placeholder='Enter text'
+                inputRef = {(textBox) => {this.textBox = textBox}}
+                onChange = { this.onSubmit } 
+              />
+            </form>
+        </div>
+        {this.state.classes.length}
+        <br/>
+        {this.state.classes.map((aClass) => {
+          console.log(aClass.name)
+          return aClass.name
+        })}
       </div>
     );
   }
